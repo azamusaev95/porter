5508b5af325c9ed2880f879c741a2730bc0f16fe
Azamat <79196607+azamusaev95@users.noreply.github.com>
2024-03-25 11:00:17 +0600

    Update script.js
    change number phone

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ const linkTags = document.querySelectorAll("whatsappGo");
 const telLink = document.getElementById("telGo");
 
 const NUMBER_1 = "996500441844";
-const NUMBER_2 = "996703660044";
+const NUMBER_2 = "996500441844";
 const linkTag = document.querySelectorAll("#whatsappGo");
 
 const min = 1;
